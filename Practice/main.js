b44b0cd66c62738de982b8918169caafabc1d804
Slava <Internationalrelat@gmail.com>
2022-07-20 19:13:10 +0200

    first commit

---
+++ Practice/main.js
@@ -0,0 +1,12 @@
+// alert('Hello, bitch');
+
+// const result = confirm("Press this button");
+// console.log(result);
+
+const category = 'toys';
+
+console.log(`https://thisurl.com/${category}/5`);
+
+const user = "Slava";
+
+alert(`Hello, ${user}`);
